4b5602df46324e4950b43dcbe95bce4c43e19b75
KyllianBeguin <kyllianbeguin@vivaldi.net>
2024-03-21 17:18:57 -0400

    [kb] 🔨 Fix table name

--- DataEngineering/mysql/schema.sql
+++ DataEngineering/mysql/schema.sql
@@ -1,3 +1,5 @@
+-- Adminer 4.8.1 MySQL 8.3.0 dump
+
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
@@ -6,7 +8,7 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 SET NAMES utf8mb4;
 
 DROP TABLE IF EXISTS `countsclean`;
-CREATE TABLE `countclean` (
+CREATE TABLE `countsclean` (
   `year` int NOT NULL,
   `month` int NOT NULL,
   `day` int NOT NULL,
@@ -14,3 +16,6 @@ CREATE TABLE `countclean` (
   `name` varchar(255) NOT NULL,
   `count` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- 2024-03-21 20:51:19
